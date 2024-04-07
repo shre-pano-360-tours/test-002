ef35c176c5b6cf527447ac66fcbaabdb0d451720
shre-pano-360-tours <josalo@gmail.com>
2024-04-07 08:41:25 +0200

    Upload

---
+++ data.js
@@ -0,0 +1,157 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-sample4",
+      "name": "sample4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 250,
+      "initialViewParameters": {
+        "yaw": -2.4237769665043007,
+        "pitch": 0.08744343664683996,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7333234780968656,
+          "pitch": -0.02901644423626948,
+          "rotation": 0,
+          "target": "3-sample1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-sample3",
+      "name": "sample3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 250,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.199160351350642,
+          "pitch": -0.008744358072029002,
+          "rotation": 0,
+          "target": "2-sample2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-sample2",
+      "name": "sample2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 250,
+      "initialViewParameters": {
+        "yaw": 1.2920335711205198,
+        "pitch": 0.1771126525842721,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.543631905889379,
+          "pitch": -0.008041543970271903,
+          "rotation": 0,
+          "target": "3-sample1"
+        },
+        {
+          "yaw": -1.674537349552823,
+          "pitch": 0.022723383918513917,
+          "rotation": 0,
+          "target": "1-sample3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-sample1",
+      "name": "sample1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": -1.194879257134584,
+        "pitch": -0.0037471401453164077,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2477464828475604,
+          "pitch": 0.07536861160916075,
+          "rotation": 0,
+          "target": "0-sample4"
+        },
+        {
+          "yaw": 1.3196944499160796,
+          "pitch": -0.010161644340808351,
+          "rotation": 0,
+          "target": "1-sample3"
+        },
+        {
+          "yaw": -2.333446234663853,
+          "pitch": -0.02498706490781899,
+          "rotation": 0,
+          "target": "2-sample2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
